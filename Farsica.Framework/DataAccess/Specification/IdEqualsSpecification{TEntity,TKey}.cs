40ae10a9c3cc54b31a0962c11b327ea7fa90937e
F.Khosravi <F.Khosravi@NC-FA-KHOSRAVI>
2023-01-21 11:56:24 +0330

    generic IdEqualsSpecification

---
+++ Farsica.Framework/DataAccess/Specification/IdEqualsSpecification{TEntity,TKey}.cs
@@ -0,0 +1,20 @@
+﻿namespace Farsica.Framework.DataAccess.Specification
+{
+    using System;
+    using System.Linq.Expressions;
+    using Farsica.Framework.DataAccess.Entities;
+
+    public sealed class IdEqualsSpecification<TEntity, TKey> : SpecificationBase<TEntity>
+        where TEntity : class, IEntity<TEntity, TKey>
+        where TKey : IEquatable<TKey>
+    {
+        private readonly TKey id;
+
+        public IdEqualsSpecification(TKey id)
+        {
+            this.id = id;
+        }
+
+        public override Expression<Func<TEntity, bool>> Expression() => t => t.Id.Equals(id);
+    }
+}
