0ec41e8313a2532bb58364291a146c5560285298
f.khosravi <f.khosravi@asax.ir>
2023-04-26 11:20:51 +0330

    mapping

---
+++ Farsica.Framework/Mapping/AdaptTwoWaysAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Farsica.Framework.Mapping
+{
+    using System;
+
+    public class AdaptTwoWaysAttribute : Mapster.AdaptTwoWaysAttribute
+    {
+        public AdaptTwoWaysAttribute(Type type)
+            : base(type)
+        {
+        }
+
+        public AdaptTwoWaysAttribute(string name)
+            : base(name)
+        {
+        }
+    }
+}
