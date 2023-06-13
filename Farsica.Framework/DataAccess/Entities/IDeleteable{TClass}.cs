c06ae001cb0d160ea9b80db3e7c6f5aaba2cf50f
f.khosravi <f.khosravi@asax.ir>
2023-06-13 11:01:01 +0330

    deleteable/enableable

---
+++ Farsica.Framework/DataAccess/Entities/IDeleteable{TClass}.cs
@@ -0,0 +1,11 @@
+﻿namespace Farsica.Framework.DataAccess.Entities
+{
+    using Farsica.Framework.DataAnnotation.Schema;
+
+    public interface IDeleteable<TClass>
+        where TClass : class
+    {
+        [NotMapped]
+        bool Deleted { get; set; }
+    }
+}
---
+++ Farsica.Framework/DataAccess/Entities/IEnableable{TClass}.cs
@@ -0,0 +1,11 @@
+﻿namespace Farsica.Framework.DataAccess.Entities
+{
+    using Farsica.Framework.DataAnnotation.Schema;
+
+    public interface IEnableable<TClass>
+        where TClass : class
+    {
+        [NotMapped]
+        bool Enabled { get; set; }
+    }
+}
