8389c86f2886309397bd1cfe731cf22446c2441a
f.khosravi <f.khosravi@asax.ir>
2023-08-21 17:17:35 +0330

    list

---
+++ Farsica.Framework/Data/ListRequestDto{T}.cs
@@ -0,0 +1,11 @@
+﻿namespace Farsica.Framework.Data
+{
+    using Farsica.Framework.DataAccess.Specification;
+
+    public class ListRequestDto<T>
+    {
+        public ISpecification<T>? Specification { get; set; }
+
+        public PagingDto? PagingDto { get; set; }
+    }
+}
