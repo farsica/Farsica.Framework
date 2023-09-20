ff7f28038d40daebfc0e35aef94833aec2e04615
f.khosravi <f.khosravi@asax.ir>
2023-09-20 13:56:06 +0330

    exist

--- Farsica.Framework/DataAnnotation/CultureAttribute.cs
+++ Farsica.Framework/DataAnnotation/CultureAttribute.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Linq;
     using Farsica.Framework.Core.Extensions.Collections.Generic;
     using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
