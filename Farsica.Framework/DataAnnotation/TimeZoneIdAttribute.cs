aab517d9fde58cffe107dfd0ad3430570395b545
Pooya Hadavi <p.hadavi@asax.ir>
2023-11-07 11:12:52 +0330

    Add TimeZoneIdAttribute

---
+++ Farsica.Framework/DataAnnotation/TimeZoneIdAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Farsica.Framework.DataAnnotation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using Farsica.Framework.TimeZone;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public sealed class TimeZoneIdAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var timeZoneId = value?.ToString();
+            if (string.IsNullOrEmpty(timeZoneId))
+            {
+                return ValidationResult.Success;
+            }
+
+            var timeZones = validationContext.GetRequiredService<ITimeZoneProvider>().GetTimeZones();
+            return timeZones?.Any(t => t.Id!.Equals(timeZoneId, StringComparison.Ordinal)) == true ? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
