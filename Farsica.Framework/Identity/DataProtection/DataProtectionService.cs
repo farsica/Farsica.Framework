c5cb08b0c5f12039e35cb2b033f69d943573f8b9
f.khosravi <f.khosravi@asax.ir>
2024-03-27 10:05:56 +0330

    DataProtectionService

--- Farsica.Framework/Identity/DataProtection/DataProtectionService.cs
+++ Farsica.Framework/Identity/DataProtection/DataProtectionService.cs
@@ -1,51 +1,26 @@
 ﻿namespace Farsica.Framework.Identity.DataProtection
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Xml.Linq;
-    using Farsica.Framework.Core;
     using Farsica.Framework.DataAccess.Audit;
     using Farsica.Framework.DataAccess.UnitOfWork;
-    using Farsica.Framework.Service;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.Extensions.Logging;
     using NUlid;
 
-    public class DataProtectionService : ServiceBase<DataProtectionService>, IDataProtectionService
+    public class DataProtectionService(IUnitOfWorkProvider unitOfWorkProvider) : IDataProtectionService
     {
-        public DataProtectionService(Lazy<IUnitOfWorkProvider> unitOfWorkProvider, Lazy<IHttpContextAccessor> httpContextAccessor, Lazy<ILogger<DataProtectionService>> logger)
-        : base(unitOfWorkProvider, httpContextAccessor, logger)
-        {
-        }
-
         public IEnumerable<XElement>? GetDataProtections()
         {
-            try
-            {
-                var uow = UnitOfWorkProvider.Value.CreateUnitOfWork();
-                return uow.GetRepository<DataProtectionKey, Ulid>().GetManyQueryable().Select(t => t.Xml).ToList().Select(XElement.Parse);
-            }
-            catch (Exception exc)
-            {
-                Logger.Value.LogException(exc);
-                return null;
-            }
+            var uow = unitOfWorkProvider.CreateUnitOfWork();
+            return uow.GetRepository<DataProtectionKey, Ulid>().GetManyQueryable().Select(t => t.Xml).ToList().Select(XElement.Parse);
         }
 
         public void AddDataProtection(DataProtectionKey dataProtectionKey)
         {
-            try
-            {
-                var uow = UnitOfWorkProvider.Value.CreateUnitOfWork();
-                var repository = uow.GetRepository<DataProtectionKey, Ulid>();
-                repository.Add(dataProtectionKey);
-                uow.SaveChanges();
-            }
-            catch (Exception exc)
-            {
-                Logger.Value.LogException(exc);
-            }
+            var uow = unitOfWorkProvider.CreateUnitOfWork();
+            var repository = uow.GetRepository<DataProtectionKey, Ulid>();
+            repository.Add(dataProtectionKey);
+            uow.SaveChanges();
         }
     }
 }
